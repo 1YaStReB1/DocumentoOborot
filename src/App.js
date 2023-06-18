a371d5ecd5cc1691312c05b60f88235adaac5995
1YaStReB1 <92804295+1YaStReB1@users.noreply.github.com>
2023-06-18 18:57:02 +0500

    названия

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Header from './components/Header/Header';
 import { PulseLoader } from 'react-spinners';
 
 function App() {
-  const words = ['sdff', '2', '3', '4', '5', '6']
-
+  const words = ['Образование', 'Спорт', 'Культура', 'Здравоохранение', 'Экономическое развитие', 'Другое']
+  
   const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
   }
@@ -15,7 +15,7 @@ function App() {
   const result = () => {
     setShow(true)
     setTimeout(() => {
-      setAnswer(words[getRandomInt(8)])
+      setAnswer(words[getRandomInt(6)])
       setShow(false)
     }, 1500)
     
@@ -25,7 +25,7 @@ function App() {
     <div className="App">
       <Header result = {result}/>
       <div className="text">Просмотр файла</div>
-      <object className='pdf__container' data="" width="500" height="500" type="application/pdf">не удалось показать документ</object>
+      <object className='pdf__container' data="" width="490" height="490" type="application/pdf">не удалось показать документ</object>
       {show ? <PulseLoader color="#36d7b7" /> : <div className="text">Класс документа: {answer}</div> }
     </div>
   );
